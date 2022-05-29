4778ee5ab36df889edb5edfdd29acf260a7ac28f
ELASY313 <ghaithnaser2009@gmail.com>
2022-05-29 20:21:21 +0300

    commit

---
+++ src/data.js
@@ -0,0 +1,84 @@
+const menu = [
+
+  {
+    id: 1,
+    title: 'buttermilk pancakes',
+    category: 'breakfast',
+    price: 15.99,
+    img:"https://www.menu.anneson.com/images/item-1.jpeg",
+    desc: ` eggy, salty, just this side of sweet. A little indulgent and yet still somehow appropriate first thing in the morning, 
+    those fluffy stacks with crisped edges, dripping with maple syrup, are everything you want, exactly when you want them `,
+  },
+  {
+    id: 2,
+    title: 'diner double',
+    category: 'lunch',
+    price: 13.99,
+    img:"https://www.menu.anneson.com/images/item-2.jpeg",
+    desc: ` Your choice of Canadian, Swiss or Cheddar cheese. Served with lettuce, 
+    tomato, red onions and pickles on a brioche bun. Served with wavy-cut French fries.`,
+  },
+  {
+    id: 3,
+    title: 'godzilla milkshake',
+    category: 'shakes',
+    price: 6.99,
+    img:"https://www.menu.anneson.com/images/item-3.jpeg",
+    desc: `Horlicks Godzilla (lots of Horlicks powder, ice cream, whipped cream and a cherry)
+    `,
+  },
+  {
+    id: 4,
+    title: 'country delight',
+    category: 'breakfast',
+    price: 20.99,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi0mrIP_lMu7wO3IXJFXS_OtWCnXdpRsdhrQ&usqp=CAU',
+    desc: `Onions, Sweet Bell Peppers & Fresh Mushrooms, Topped with Shredded Marble Cheese & 2 Eggs Served Your Way.
+     Served with a Side of Smoked Bacon, Ham OR Link Sausage. `,
+  },
+  {
+    id: 5,
+    title: 'egg attack',
+    category: 'lunch',
+    price: 22.99,
+    img:"https://www.menu.anneson.com/images/item-5.jpeg",
+    desc: `Sunnyside up eggs is a go-to dish for your hunger attack. You can make it more interesting by adding a few tossed veggies to give you a fuller tummy. Honestly, they are super easy to make with very few ingredients.
+
+    `,
+  },
+  {
+    id: 6,
+    title: 'oreo dream',
+    category: 'shakes',
+    price: 18.99,
+    img:"https://www.menu.anneson.com/images/item-6.jpeg",
+    desc: `Delicious chocolatey decadence in every bite. This copycat recipe for the Cheesecake Factory’s Oreo Dream Extreme Cheesecake is a chocolate lover’s dream come to life!
+
+    `,
+  },
+  {
+    id: 7,
+    title: 'Burger',
+    category: 'breakfast',
+    price: 8.99,
+    img:"https://www.kindpng.com/picc/m/5-55513_berger-food-png-chicken-burger-hd-png-transparent.png",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 8,
+    title: 'american classic',
+    category: 'lunch',
+    price: 12.99,
+    img:"https://www.menu.anneson.com/images/item-8.jpeg",
+    desc: ` american classic lunch. eggs, burger and french fries at plate`,
+  },
+  {
+    id: 9,
+    title: 'quarantine buddy',
+    category: 'shakes',
+    price: 16.99,
+    img:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRXvHXtipy9ryQeA_uZJlp-Hmw75B41lynmJTOWXjnoPIWq4LGl",
+    desc: `Chocolate shake, peanut butter, peanuts, chocolate sauce & whipped cream. You’d be nuts to miss out on this one`,
+  },
+];
+export default menu;
